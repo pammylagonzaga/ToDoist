2bce4473a5c3d8a63de2194b2dd846b2fad1e0f3
Pammyla Gonzaga <pammyla.gonzaga@gmail.com>
2026-02-05 16:51:26 -0300

    Aula III - BottonRoutes

---
+++ src/pages/list/styles.ts
@@ -0,0 +1,9 @@
+import React, { forwardRef } from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+import { themes } from "../../global/themes";
+
+
+export const style = StyleSheet.create({
+
+    
+})
---
+++ src/routes/botton.routes.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, Settings } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import List from '../pages/list';
+import User from '../pages/user';
+import CustomTabBar from '../components/CustomTabBar';
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomRoutes(){
+    return (
+        <Tab.Navigator
+        screenOptions={{
+            headerShown:false
+        }}
+        tabBar={props=><CustomTabBar{...props}/>}
+        >
+            <Tab.Screen 
+                name="List" 
+                component={List}
+            />
+            <Tab.Screen 
+                name="User" 
+                component={User}
+            />
+        </Tab.Navigator>
+    )
+}
